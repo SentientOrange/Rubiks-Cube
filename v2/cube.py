6601f775d15578d44bf2fbbdd7de482008bbc73b
JuiMin <d95wang@gmail.com>
2018-07-29 23:45:41 -0700

    restructure for the new version

---
+++ v2/cube.py
@@ -0,0 +1,10 @@
+# Derek Wang
+
+# Rubiks Cube Representation
+
+# In order to do this project, we need to define a Rubiks Cube and all provide state management
+# An agent should be able to perform any of the different rotations which will allow the agent to solve the cube.
+
+# We will construct this with a 3x3x3 traditional Rubik's Cube in mind
+
+
