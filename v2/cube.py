87167c8ae551e94b238efb65dc7e2b43e48b5380
Derek J Wang <d95wang@uw.edu>
2018-08-23 17:16:59 -0600

    add face rotation

--- v2/cube.py
+++ v2/cube.py
@@ -9,14 +9,25 @@
 
 class Cube:
    # Init the cube using a give size
-   def __init__(self, size=3):
-      self.size = size
-      self.front = [[0 for x in range(size)] for x in range(size)]
-      self.left = [[1 for x in range(size)] for x in range(size)]
-      self.back = [[2 for x in range(size)] for x in range(size)]
-      self.top = [[3 for x in range(size)] for x in range(size)]
-      self.right = [[4 for x in range(size)] for x in range(size)]
-      self.bottom = [[5 for x in range(size)] for x in range(size)]
+   def __init__(self):
+      self.size = 3
+      self.front = [[0 for x in range(self.size)] for x in range(self.size)]
+      self.left = [[1 for x in range(self.size)] for x in range(self.size)]
+      self.back = [[2 for x in range(self.size)] for x in range(self.size)]
+      self.top = [[3 for x in range(self.size)] for x in range(self.size)]
+      self.right = [[4 for x in range(self.size)] for x in range(self.size)]
+      self.bottom = [[5 for x in range(self.size)] for x in range(self.size)]
+
+   def rotate_face(self, matrix):
+      """
+      Takes in the 2 dimentional face representation and performs the matrix operation of clockwise rotation
+      The new matrix is returned
+      """
+      temp = [[],[],[]] # Generate the new matrix
+      for x in reversed(range(self.size)):
+         for y in range(self.size):
+            temp[y].append(matrix[x][y])
+      return temp
 
    def rotate_front(self):
       """
