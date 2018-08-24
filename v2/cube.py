5645c55584d2cbe0478a8ff4736a59a1a6f949b5
JuiMin <d95wang@gmail.com>
2018-08-24 09:06:48 -0700

    add the test and cube edits so that the class variables are hidden

--- v2/cube.py
+++ v2/cube.py
@@ -10,49 +10,86 @@
 class Cube:
    # Init the cube using a give size
    def __init__(self):
-      self.size = 3
-      self.front = [[0 for x in range(self.size)] for x in range(self.size)]
-      self.left = [[1 for x in range(self.size)] for x in range(self.size)]
-      self.back = [[2 for x in range(self.size)] for x in range(self.size)]
-      self.top = [[3 for x in range(self.size)] for x in range(self.size)]
-      self.right = [[4 for x in range(self.size)] for x in range(self.size)]
-      self.bottom = [[5 for x in range(self.size)] for x in range(self.size)]
-
+      self.__size__ = 3
+      self.__front__ = [[0 for x in range(self.__size__)] for x in range(self.__size__)]
+      self.__left__ = [[1 for x in range(self.__size__)] for x in range(self.__size__)]
+      self.__back__ = [[2 for x in range(self.__size__)] for x in range(self.__size__)]
+      self.__top__ = [[3 for x in range(self.__size__)] for x in range(self.__size__)]
+      self.__right__ = [[4 for x in range(self.__size__)] for x in range(self.__size__)]
+      self.__bottom__ = [[5 for x in range(self.__size__)] for x in range(self.__size__)]
+      
    def set_front(self, l):
        if len(l) == 3 & len(l[0]) == 3 & len(l[1]) == 3 & len(l[2]) == 3:
-          self.front = l
+          self.__front__ = l
        else:
            raise Exception("Input lists incorrect length")
 
    def set_left(self, l):
        if len(l) == 3 & len(l[0]) == 3 & len(l[1]) == 3 & len(l[2]) == 3:
-          self.left = l
+          self.__left__ = l
        else:
            raise Exception("Input lists incorrect length")
 
    def set_back(self, l):
        if len(l) == 3 & len(l[0]) == 3 & len(l[1]) == 3 & len(l[2]) == 3:
-          self.back = l
+          self.__back__ = l
        else:
            raise Exception("Input lists incorrect length")
 
    def set_top(self, l):
        if len(l) == 3 & len(l[0]) == 3 & len(l[1]) == 3 & len(l[2]) == 3:
-          self.top = l
+          self.__top__ = l
        else:
            raise Exception("Input lists incorrect length")
 
    def set_right(self, l):
        if len(l) == 3 & len(l[0]) == 3 & len(l[1]) == 3 & len(l[2]) == 3:
-          self.right = l
+          self.__right__ = l
        else:
            raise Exception("Input lists incorrect length")
 
    def set_bottom(self, l):
        if len(l) == 3 & len(l[0]) == 3 & len(l[1]) == 3 & len(l[2]) == 3:
-          self.bottom = l
+          self.__bottom__ = l
        else:
            raise Exception("Input lists incorrect length")
+
+   def get_front(self):
+      """
+      Get the front face of the cube
+      """
+      return self.__front__
+   
+   def get_back(self):
+      """
+      Get the back face of the cube
+      """
+      return self.__back__
+   
+   def get_left(self):
+      """
+      Get the back face of the cube
+      """
+      return self.__left__
+
+   def get_right(self):
+      """
+      Get the back face of the cube
+      """
+      return self.__right__
+
+   def get_top(self):
+      """
+      Get the back face of the cube
+      """
+      return self.__top__
+
+   def get_bottom(self):
+      """
+      Get the back face of the cube
+      """
+      return self.__bottom__
+
 
    def rotate_face(self, matrix):
       """
@@ -60,8 +97,8 @@ class Cube:
       The new matrix is returned
       """
       temp = [[],[],[]] # Generate the new matrix
-      for x in reversed(range(self.size)):
-         for y in range(self.size):
+      for x in reversed(range(self.__size__)):
+         for y in range(self.__size__):
             temp[y].append(matrix[x][y])
       return temp
 
@@ -70,97 +107,97 @@ class Cube:
       Rotate front face of the cube
       """
       # Rotate the face itself clockwise
-      self.front = self.rotate_face(self.front)
+      self.__front__ = self.rotate_face(self.__front__)
       # Perform the rotations on the edges
       # The top and bottom edges can save values before transfer
-      new_top = [self.left[row][-1] for row in reversed(range(self.size))]
-      new_bottom = [self.right[row][0] for row in reversed(range(self.size))]
+      new_top = [self.__left__[row][-1] for row in reversed(range(self.__size__))]
+      new_bottom = [self.__right__[row][0] for row in reversed(range(self.__size__))]
       # Transfer top and bottom to the sides
       # Top to right
-      for idx in range(self.size):
-         self.right[idx][0] = self.top[-1][idx]
+      for idx in range(self.__size__):
+         self.__right__[idx][0] = self.__top__[-1][idx]
 
       # Bottom to left
-      for idx in range(self.size):
-         self.left[idx][-1] = self.bottom[0][idx]
+      for idx in range(self.__size__):
+         self.__left__[idx][-1] = self.__bottom__[0][idx]
       
       # Replace the top and bottom
-      self.top[-1] = new_top
-      self.bottom[0] = new_bottom
+      self.__top__[-1] = new_top
+      self.__bottom__[0] = new_bottom
       
    def rotate_back(self):
       """
       Rotate back face of the cube
       """
       # Rotate the face itself clockwise
-      self.back = self.rotate_face(self.back)
+      self.__back__ = self.rotate_face(self.__back__)
 
-      new_top = [self.right[row][-1] for row in range(self.size)]
-      new_bottom = [self.left[row][0] for row in range(self.size)]
+      new_top = [self.__right__[row][-1] for row in range(self.__size__)]
+      new_bottom = [self.__left__[row][0] for row in range(self.__size__)]
       # Transfer top and bottom to the sides
       # Top to right face in this orientation
-      for idx in range(self.size):
-         self.left[idx][0] = self.top[0][-1 - idx]
+      for idx in range(self.__size__):
+         self.__left__[idx][0] = self.__top__[0][-1 - idx]
 
       # Bottom to left
-      for idx in range(self.size):
-         self.right[idx][-1] = self.bottom[-1][-1 - idx]
+      for idx in range(self.__size__):
+         self.__right__[idx][-1] = self.__bottom__[-1][-1 - idx]
    
       # Replace the top and bottom
-      self.top[0] = new_top
-      self.bottom[-1] = new_bottom
+      self.__top__[0] = new_top
+      self.__bottom__[-1] = new_bottom
 
    def rotate_left(self):
       """
       Rotate left face of the cube
       """
       # Rotate the face itself clockwise
-      self.left = self.rotate_face(self.left)
+      self.__left__ = self.rotate_face(self.__left__)
 
       # Grab the actual data being moved
-      new_top = [self.back[row][-1] for row in reversed(range(self.size))]
-      new_bottom = [self.front[row][0] for row in reversed(range(self.size))]
+      new_top = [self.__back__[row][-1] for row in reversed(range(self.__size__))]
+      new_bottom = [self.__front__[row][0] for row in reversed(range(self.__size__))]
 
-      new_right = [self.top[idx][0] for idx in range(self.size)]
-      new_left = [self.bottom[idx][0] for idx in reversed(range(self.size))]
+      new_right = [self.__top__[idx][0] for idx in range(self.__size__)]
+      new_left = [self.__bottom__[idx][0] for idx in reversed(range(self.__size__))]
 
       # Transfer top and bottom to the sides
       # Top to right face in this orientation
-      for idx in range(self.size):
+      for idx in range(self.__size__):
          # Back to top
-         self.top[idx][0] = new_top[idx]
+         self.__top__[idx][0] = new_top[idx]
          # Front to bottom
-         self.bottom[idx][0] = new_bottom[::-1][idx]
+         self.__bottom__[idx][0] = new_bottom[::-1][idx]
          # top to front
-         self.front[idx][0] = new_right[idx]
+         self.__front__[idx][0] = new_right[idx]
          # bottom to back
-         self.back[idx][-1] = new_left[idx]
+         self.__back__[idx][-1] = new_left[idx]
 
    def rotate_right(self):
       """
       Rotate right side clockwise
       """
       # Rotate the face itself clockwise  
-      self.right = self.rotate_face(self.right)
+      self.__right__ = self.rotate_face(self.__right__)
 
       # Grab the actual data being moved
-      new_top = [self.front[row][-1] for row in range(self.size)]
-      new_bottom = [self.back[row][0] for row in reversed(range(self.size))]
+      new_top = [self.__front__[row][-1] for row in range(self.__size__)]
+      new_bottom = [self.__back__[row][0] for row in reversed(range(self.__size__))]
 
-      new_right = [self.top[idx][-1] for idx in reversed(range(self.size))]
-      new_left = [self.bottom[idx][-1] for idx in range(self.size)]
+      new_right = [self.__top__[idx][-1] for idx in reversed(range(self.__size__))]
+      new_left = [self.__bottom__[idx][-1] for idx in range(self.__size__)]
 
       # Transfer top and bottom to the sides
       # Top to right face in this orientation
-      for idx in range(self.size):
+      for idx in range(self.__size__):
          # Back to top
-         self.top[idx][-1] = new_top[idx]
+         self.__top__[idx][-1] = new_top[idx]
          # Front to bottom
-         self.bottom[idx][-1] = new_bottom[idx]
+         self.__bottom__[idx][-1] = new_bottom[idx]
          # top to front
-         self.front[idx][-1] = new_left[idx]
+         self.__front__[idx][-1] = new_left[idx]
          # bottom to back
-         self.back[idx][0] = new_right[idx]
+         self.__back__[idx][0] = new_right[idx]
 
    def rotate_top(self):
       """
@@ -168,14 +205,14 @@ class Cube:
       """
       #grab bottom row of front, left, right, back
       temp = []
-      temp = self.front[0]
-      self.front[0] = self.right[0]
-      self.right[0] = self.back[0]
-      self.back[0] = self.left[0]
-      self.left[0] = temp
+      temp = self.__front__[0]
+      self.__front__[0] = self.__right__[0]
+      self.__right__[0] = self.__back__[0]
+      self.__back__[0] = self.__left__[0]
+      self.__left__[0] = temp
 
       # Rotate the face itself clockwisez 
-      self.top = self.rotate_face(self.top)
+      self.__top__ = self.rotate_face(self.__top__)
 
    def rotate_bottom(self):
       """
@@ -183,28 +220,61 @@ class Cube:
       """
       #grab bottom row of front, left, right, back
       temp = []
-      temp = self.front[2]
-      self.front[2] = self.left[2]
-      self.left[2] = self.back[2]
-      self.back[2] = self.right[2]
-      self.right[2] = temp
+      temp = self.__front__[2]
+      self.__front__[2] = self.__left__[2]
+      self.__left__[2] = self.__back__[2]
+      self.__back__[2] = self.__right__[2]
+      self.__right__[2] = temp
       # Rotate the face itself clockwise
-      self.bottom = self.rotate_face(self.bottom)
+      self.__bottom__ = self.rotate_face(self.__bottom__)
 
    def get_size(self):
    	"""
 		Returns the n dimension of the cube
 		"""
-   	return self.size
+   	return self.__size__
 
    def __printer__(self, top, left, front, right, back, bottom):
+      """
+      Print the faces with the orientation of 
+
+         T
+      L  F  R  Ba
+         Bo   
+      """
       for row in top:
          print("          " + str(row))
-      for idx in range(self.size):
+      for idx in range(self.__size__):
          print(str(left[idx]) + " " + str(front[idx]) + " " + str(right[idx]) + " " + str(back[idx]))
       for row in bottom:
          print("          " + str(row)) 
 
    def print_cube(self, orientation="front"):
+      """
+      Switch the print case depending on the given orientation
+      """
       if orientation == "front":
-         self.__printer__(self.top, self.left, self.front, self.right, self.back, self.bottom)
+         self.__printer__(self.__top__, self.__left__, self.__front__, self.__right__, self.__back__, self.__bottom__)
+      # TODO: Put in the methods for orientation of different faces being the front
+      # This probably requires the use of matrix rotations for the top and bottom faces
+
+   def is_solved(self):
+      """
+      Checks that the rubik's cube is solved. This can mean that each face has the same numeral in the 3x3 matrix
+      """
+      solved = True
+      if self.__front__ != [[self.__front__[0][0] for x in range(self.__size__)] for x in range(self.__size__)]:
+         solved = False
+      if self.__back__ != [[self.__back__[0][0] for x in range(self.__size__)] for x in range(self.__size__)]:
+         solved = False
+      if self.__left__ != [[self.__left__[0][0] for x in range(self.__size__)] for x in range(self.__size__)]:
+         solved = False
+      if self.__right__ != [[self.__right__[0][0] for x in range(self.__size__)] for x in range(self.__size__)]:
+         solved = False
+      if self.__top__ != [[self.__top__[0][0] for x in range(self.__size__)] for x in range(self.__size__)]:
+         solved = False
+      if self.__bottom__ != [[self.__bottom__[0][0] for x in range(self.__size__)] for x in range(self.__size__)]:
+         solved = False
+      # If we had no issues then the cube should be solved
+      return solved
+      
--- v2/test_cube.py
+++ v2/test_cube.py
@@ -17,12 +17,12 @@ def test_cube_generation():
    Test that the cube initialized properly 
    """
    c = cube.Cube()
-   assert c.front == [[0 for x in range(3)] for y in range(3)]
-   assert c.left == [[1 for x in range(3)] for y in range(3)]
-   assert c.back == [[2 for x in range(3)] for y in range(3)]
-   assert c.top == [[3 for x in range(3)] for y in range(3)]
-   assert c.right == [[4 for x in range(3)] for y in range(3)]
-   assert c.bottom == [[5 for x in range(3)] for y in range(3)]
+   assert c.get_front() == [[0 for x in range(3)] for y in range(3)]
+   assert c.get_left() == [[1 for x in range(3)] for y in range(3)]
+   assert c.get_back() == [[2 for x in range(3)] for y in range(3)]
+   assert c.get_top() == [[3 for x in range(3)] for y in range(3)]
+   assert c.get_right() == [[4 for x in range(3)] for y in range(3)]
+   assert c.get_bottom() == [[5 for x in range(3)] for y in range(3)]
 
 def test_cube_frontrotation():
    """
@@ -39,7 +39,7 @@ def test_rotate_face():
    clockwise_rotation = [[2,2,1],[3,3,2],[9,1,7]]
    assert c.rotate_face(init) == clockwise_rotation
 
-def test_rotate_front():
+def test_rotate_front_basic():
    """
    Test the front rotation method
    """
@@ -47,17 +47,17 @@ def test_rotate_front():
    # Perform the turn
    c.rotate_front()
    # The front face on initialization rotation should be the same
-   assert c.front == [[0 for x in range(3)] for y in range(3)]
+   assert c.get_front() == [[0 for x in range(3)] for y in range(3)]
 
    # Assert the edges for the edges are correct
    # Test
    for idx in range(3):
-      assert c.left[idx][-1] == 5
-      assert c.top[-1][idx] == 1
-      assert c.bottom[0][idx] == 4
-      assert c.right[idx][0] == 3
+      assert c.get_left()[idx][-1] == 5
+      assert c.get_top()[-1][idx] == 1
+      assert c.get_bottom()[0][idx] == 4
+      assert c.get_right()[idx][0] == 3
    
-def test_rotate_back():
+def test_rotate_back_basic():
    """
    Test the front rotation method
    """
@@ -65,81 +65,79 @@ def test_rotate_back():
    # Perform the turn
    c.rotate_back()
    # The front face on initialization rotation should be the same
-   assert c.back == [[2 for x in range(3)] for y in range(3)]
+   assert c.get_back() == [[2 for x in range(3)] for y in range(3)]
 
    # Assert the edges for the edges are correct
    # Test
    for idx in range(3):
-      assert c.left[idx][0] == 3
-      assert c.top[0][idx] == 4
-      assert c.bottom[-1][idx] == 1
-      assert c.right[idx][-1] == 5
+      assert c.get_left()[idx][0] == 3
+      assert c.get_top()[0][idx] == 4
+      assert c.get_bottom()[-1][idx] == 1
+      assert c.get_right()[idx][-1] == 5
 
 def test_rotate_bottom():
     c = cube.Cube()
-    c.rotate_face = MagicMock(return_value=c.bottom)
+    c.rotate_face = MagicMock(return_value=c.get_bottom())
     c.rotate_bottom()
-    assert c.front == [[0,0,0],[0,0,0],[1,1,1]]
-    assert c.left == [[1,1,1],[1,1,1],[2,2,2]]
-    assert c.back == [[2,2,2],[2,2,2],[4,4,4]]
-    assert c.right == [[4,4,4],[4,4,4],[0,0,0]]
-    assert c.top == [[3,3,3],[3,3,3],[3,3,3]]
-    assert c.bottom == [[5,5,5],[5,5,5],[5,5,5]]
-
-
+    assert c.get_front() == [[0,0,0],[0,0,0],[1,1,1]]
+    assert c.get_left() == [[1,1,1],[1,1,1],[2,2,2]]
+    assert c.get_back() == [[2,2,2],[2,2,2],[4,4,4]]
+    assert c.get_right() == [[4,4,4],[4,4,4],[0,0,0]]
+    assert c.get_top() == [[3,3,3],[3,3,3],[3,3,3]]
+    assert c.get_bottom() == [[5,5,5],[5,5,5],[5,5,5]]
 
 def test_rotate_top():
     c = cube.Cube()
-    c.rotate_face = MagicMock(return_value=c.top)
+    c.rotate_face = MagicMock(return_value=c.get_top())
     c.rotate_top()
-    assert c.front == [[4,4,4],[0,0,0],[0,0,0]]
-    assert c.left == [[0,0,0],[1,1,1],[1,1,1]]
-    assert c.back == [[1,1,1],[2,2,2],[2,2,2]]
-    assert c.right == [[2,2,2],[4,4,4],[4,4,4]]
-    assert c.top == [[3,3,3],[3,3,3],[3,3,3]]
-    assert c.bottom == [[5,5,5],[5,5,5],[5,5,5]]
+    assert c.get_front() == [[4,4,4],[0,0,0],[0,0,0]]
+    assert c.get_left() == [[0,0,0],[1,1,1],[1,1,1]]
+    assert c.get_back() == [[1,1,1],[2,2,2],[2,2,2]]
+    assert c.get_right() == [[2,2,2],[4,4,4],[4,4,4]]
+    assert c.get_top() == [[3,3,3],[3,3,3],[3,3,3]]
+    assert c.get_bottom() == [[5,5,5],[5,5,5],[5,5,5]]
 
 def test_rotate_front():
     c = cube.Cube()
-    c.rotate_face = MagicMock(return_value=c.front)
+    c.rotate_face = MagicMock(return_value=c.get_front())
     c.rotate_front()
-    assert c.top == [[3,3,3],[3,3,3],[1,1,1]]
-    assert c.left == [[1,1,5],[1,1,5],[1,1,5]]
-    assert c.bottom == [[4,4,4],[5,5,5],[5,5,5]]
-    assert c.right == [[3,4,4],[3,4,4],[3,4,4]]
-    assert c.front == [[0,0,0],[0,0,0],[0,0,0]]
-    assert c.back == [[2,2,2],[2,2,2],[2,2,2]]
+    assert c.get_top() == [[3,3,3],[3,3,3],[1,1,1]]
+    assert c.get_left() == [[1,1,5],[1,1,5],[1,1,5]]
+    assert c.get_bottom() == [[4,4,4],[5,5,5],[5,5,5]]
+    assert c.get_right() == [[3,4,4],[3,4,4],[3,4,4]]
+    assert c.get_front() == [[0,0,0],[0,0,0],[0,0,0]]
+    assert c.get_back() == [[2,2,2],[2,2,2],[2,2,2]]
 
 def test_rotate_back():
     c = cube.Cube()
-    c.rotate_face = MagicMock(return_value=c.back)
+    c.rotate_face = MagicMock(return_value=c.get_back())
     c.rotate_back()
-    assert c.top == [[4,4,4],[3,3,3],[3,3,3]]
-    assert c.left == [[3,1,1],[3,1,1],[3,1,1]]
-    assert c.bottom == [[5,5,5],[5,5,5],[1,1,1]]
-    assert c.right == [[4,4,5],[4,4,5],[4,4,5]]
-    assert c.front == [[0,0,0],[0,0,0],[0,0,0]]
-    assert c.back == [[2,2,2],[2,2,2],[2,2,2]]
+    assert c.get_top() == [[4,4,4],[3,3,3],[3,3,3]]
+    assert c.get_left() == [[3,1,1],[3,1,1],[3,1,1]]
+    assert c.get_bottom() == [[5,5,5],[5,5,5],[1,1,1]]
+    assert c.get_right() == [[4,4,5],[4,4,5],[4,4,5]]
+    assert c.get_front() == [[0,0,0],[0,0,0],[0,0,0]]
+    assert c.get_back() == [[2,2,2],[2,2,2],[2,2,2]]
 
 def test_rotate_right():
     c = cube.Cube()
-    c.rotate_face = MagicMock(return_value=c.right)
+    c.rotate_face = MagicMock(return_value=c.get_right())
     c.rotate_right()
-    assert c.top == [[3,3,0],[3,3,0],[3,3,0]]
-    assert c.bottom == [[5,5,2],[5,5,2],[5,5,2]]
-    assert c.front == [[0,0,5],[0,0,5],[0,0,5]]
-    assert c.back == [[3,2,2],[3,2,2],[3,2,2]]
-    assert c.left == [[1,1,1],[1,1,1],[1,1,1]]
-    assert c.right == [[4,4,4],[4,4,4],[4,4,4]]
+    assert c.get_top() == [[3,3,0],[3,3,0],[3,3,0]]
+    assert c.get_bottom() == [[5,5,2],[5,5,2],[5,5,2]]
+    assert c.get_front() == [[0,0,5],[0,0,5],[0,0,5]]
+    assert c.get_back() == [[3,2,2],[3,2,2],[3,2,2]]
+    assert c.get_left() == [[1,1,1],[1,1,1],[1,1,1]]
+    assert c.get_right() == [[4,4,4],[4,4,4],[4,4,4]]
 
 def test_rotate_left():
     c = cube.Cube()
-    c.rotate_face = MagicMock(return_value=c.left)
+    c.rotate_face = MagicMock(return_value=c.get_left())
     c.rotate_left()
-    assert c.top == [[2,3,3],[2,3,3],[2,3,3]]
-    assert c.bottom == [[0,5,5],[0,5,5],[0,5,5]]
-    assert c.front == [[3,0,0],[3,0,0],[3,0,0]]
-    assert c.back == [[2,2,5],[2,2,5],[2,2,5]]
-    assert c.left == [[1,1,1],[1,1,1],[1,1,1]]
-    assert c.right == [[4,4,4],[4,4,4],[4,4,4]]
+    assert c.get_top() == [[2,3,3],[2,3,3],[2,3,3]]
+    assert c.get_bottom() == [[0,5,5],[0,5,5],[0,5,5]]
+    assert c.get_front() == [[3,0,0],[3,0,0],[3,0,0]]
+    assert c.get_back() == [[2,2,5],[2,2,5],[2,2,5]]
+    assert c.get_left() == [[1,1,1],[1,1,1],[1,1,1]]
+    assert c.get_right() == [[4,4,4],[4,4,4],[4,4,4]]
 
