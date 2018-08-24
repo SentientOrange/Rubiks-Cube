fad6a7190aa906d9fa983d57addb5b85747356e7
JuiMin <d95wang@gmail.com>
2018-08-24 09:13:51 -0700

    minor fix

--- v2/sample_run.py
+++ v2/sample_run.py
@@ -13,11 +13,17 @@ agent = a.Agent(cube)
 actions = agent.get_actions()
 
 # See initial cube state
+print('Initial State')
+print()
 cube.print_cube()
+print()
 
 # Loop through the actions and see what we get
 for action in actions:
     # Do a turn and then print the result
+    print('Performing', action, 'rotation')
+    print()
     agent.perform_action(action)
     cube.print_cube()
+    print()
 
