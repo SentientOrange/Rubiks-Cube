70e05485f968b7397db0bf81dd85f95dddbc72d0
Eric Eckert <eric95@uw.edu>
2018-08-23 17:06:51 -0700

    added unit test for rotate bottom

--- v2/test_cube.py
+++ v2/test_cube.py
@@ -2,7 +2,14 @@
 
 import pytest
 import cube
+import unittest.mock
 
 def test_cube_size():
    c = cube.Cube()
    assert c.get_size() == 3
+
+def test_rotate_bottom():
+    c = cube.Cube()
+    c.rotate_face = MagicMock(return_value=c.bottom)
+    c.rotate_bottom()
+    assert c.front == [[0,0,0],[0,0,0],[1,1,1]]
