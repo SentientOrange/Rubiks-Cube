9f583776234916e42ba4d207abd9ca37e7bca56f
Eric Eckert <eric95@uw.edu>
2018-08-23 18:20:02 -0700

    wrote rotate top unit test

--- v2/test_cube.py
+++ v2/test_cube.py
@@ -14,3 +14,26 @@ def test_rotate_bottom():
     c.rotate_face = MagicMock(return_value=c.bottom)
     c.rotate_bottom()
     assert c.front == [[0,0,0],[0,0,0],[1,1,1]]
+    assert c.left == [[1,1,1],[1,1,1],[2,2,2]]
+    assert c.back == [[2,2,2],[2,2,2],[4,4,4]]
+    assert c.right == [[4,4,4],[4,4,4],[0,0,0]]
+
+
+
+def test_rotate_top():
+    c = cube.Cube()
+    c.rotate_face = MagicMock(return_value=c.top)
+    c.rotate_top()
+    assert c.front == [[4,4,4],[0,0,0],[0,0,0]]
+    assert c.left == [[0,0,0],[1,1,1],[1,1,1]]
+    assert c.back == [[1,1,1],[2,2,2],[2,2,2]]
+    assert c.right == [[2,2,2],[4,4,4],[4,4,4]]
+
+def test_rotate_front():
+    c = cube.Cube()
+    c.rotate_face = MagicMock(return_value=c.front)
+    c.rotate_top()
+    assert c.front == [[4,4,4],[0,0,0],[0,0,0]]
+    assert c.left == [[0,0,0],[1,1,1],[1,1,1]]
+    assert c.back == [[1,1,1],[2,2,2],[2,2,2]]
+    assert c.right == [[2,2,2],[4,4,4],[4,4,4]]
