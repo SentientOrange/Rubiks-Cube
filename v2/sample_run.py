476a324919ee2afc36491784591af37b57c09374
JuiMin <d95wang@gmail.com>
2018-08-24 09:12:07 -0700

    add the sample run so i don't have to write the same thing over and over again to see that something works

---
+++ v2/sample_run.py
@@ -0,0 +1,23 @@
+# This is just a script that runs the cube and does some rotations
+# We can use this to see how each rotation works and can use it as a check to make sure that the cube rotates properly
+
+import cube as c
+import agent as a
+
+# Cube
+cube = c.Cube()
+
+# Agent
+agent = a.Agent(cube)
+
+actions = agent.get_actions()
+
+# See initial cube state
+cube.print_cube()
+
+# Loop through the actions and see what we get
+for action in actions:
+    # Do a turn and then print the result
+    agent.perform_action(action)
+    cube.print_cube()
+
