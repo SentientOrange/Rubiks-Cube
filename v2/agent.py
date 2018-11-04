ff0eea9cf3da9fe34c666a048b9e3b14c9394bd8
Derek Wang <d95wang@gmail.com>
2018-11-04 21:38:33 +0800

    add agent basis for q learning functionality. state representation added

--- v2/agent.py
+++ v2/agent.py
@@ -8,9 +8,12 @@ class Agent:
       The reference to a Rubik's cube must be passed in
       """
       # All actions are performed in a clockwise rotation of one of six sides
-      self.actions = ['front', 'back', 'left', 'right', 'top', 'bottom']
+      self.actions = ['front', 'back', 'left', 'right', 'top', 'bottom', 'none'] # include no move
       # The memory representaiton of states
       self.q_table = {}
+
+      # Epsilon for greedy function
+      self.epsilon = 1
 
    def get_actions(self):
       """
@@ -35,15 +38,55 @@ class Agent:
       elif action == 'bottom':
          cube.rotate_bottom()
          
-   def solve(self, cube):
+   def solve(self, cube, threshold=20):
       """
       Utilize some method of solving a rubik's cube and looks to solve it.
       """
-      pass
-   
-   def heuristic(self, cube):
+      # Optimal solutions show cubes can be solved in less than 20 moves. You can change this value if needed
+      for turn in range(threshold):
+         # Get state representation
+         state = self.get_state_rep(cube)
+         # Select the best move from the q table
+         
+
+         # Apply the move to the cube
+
+         # By the end of this, the cube should be completed
+
+
+   def get_state_rep(self, cube):
       """
-      Heuristic characteristic to this agent
-      Takes in a verbose argument. If this is true, we should print out the heuristic evaluations per state was we go
+      Define a state representation
       """
+      # In this experiment, we can try using the count of correct units in each face as a state
+      # this is 9^6 states which actually isn't that many
+      faces = [
+         cube.get_front(),
+         cube.get_back(),
+         cube.get_left(),
+         cube.get_right(),
+         cube.get_top(),
+         cube.get_bottom()
+      ]
+      
+      rep = []
+
+      for face in faces:
+         count = 0
+         val = face[1][1]
+         for x in range(3):
+            for y in range(3):
+               if face[x][y] == val:
+                  count += 1
+         rep.append(count)
+
+      return rep
+
+   def reward_heuristic(self, cube, action):
       pass
+
+   def q(self, state, action):
+      """
+      Generate the q value for the table using the given action and cube
+      """
+      old_q = self.q_table[(state, action)]
--- v2/solver.py
+++ v2/solver.py
@@ -14,9 +14,29 @@ TEST_CASES = 1000
 
 SCRAMBLE_MOVES = 20
 
-# Build the q learning table
+# Build the q learning table training the agent
+for case in range(TRAIN_CASES):
+    # Scramble the cube
+    cube = c.Cube()
+    cube.scramble(SCRAMBLE_MOVES)
+
+    # The agent should attempt to solve the cube.
+    agent.solve(cube)
+
+print("Agent trained on",TRAIN_CASES,"training cases")
+print()
+
+successes = 0
+
 for case in range(TEST_CASES):
     # Scramble the cube
     cube = c.Cube()
     cube.scramble(SCRAMBLE_MOVES)
-    
+
+    # The agent should attempt to solve the cube.
+    agent.solve(cube)
+
+    if (cube.is_solved()):
+        successes += 1
+
+print("Out of",TEST_CASES,"test cases, the agent solved", successes)
